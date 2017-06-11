bdd8b52855902b1c5b8508adb37cbd2eaf2bae78
Vijeinath Tissaveerasingham <vijeinath.tissaveerasingham@students.fhnw.ch>
2017-06-11 17:33:01 +0200

    alert before deleting photo
    alert before deleting photo

--- pocketCloset/src/pages/clothes/clothes.ts
+++ pocketCloset/src/pages/clothes/clothes.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'clothes.html'
@@ -13,13 +14,35 @@ export class ClothesPage {
   category_key: any;
   categories: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public params: NavParams, public db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+  constructor(public navCtrl: NavController, public params: NavParams, public db: AngularFireDatabase, public afAuth: AngularFireAuth, public alertCtrl: AlertController) {
     this.categories = this.db.list('categories');
     this.images = params.get('images');
     this.category_key = params.get('category_key');
   }
 
   deletePhoto(imageData){
+    let confirm = this
+      .alertCtrl
+      .create({
+        title: 'Sure you want to delete this photo? You cannot undo it!',
+        message: '',
+        buttons: [
+          {
+            text: 'No',
+            handler: () => {
+            }
+          }, {
+            text: 'Yes',
+            handler: () => {
+              this.delete(imageData);
+            }
+          }
+        ]
+      });
+    confirm.present();
+  }
+
+  delete(imageData){
     let subcription = this.categories.subscribe(
       competitors => {
           competitors.map(competitor => {
